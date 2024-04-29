54cef73584a00d96ead5cbb51aa02dc489cb929c
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-29 17:50:10 -0300

    feat: Add JSON middleware and updated route handlers
    Add JSON middleware to parse requests with the "Content-Type" header set to "application/json". Additionally, updated the route handlers to use the req.body provided by the JSON middleware when handling POST requests. This ensures that request body data is properly parsed as JSON and available for use in routes.

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const myExpress = require("./myExpress");
+const json = require("./json");
 
 const data = [1, 2, 3];
 
 const app = myExpress();
+app.use(json);
 
 app.get("/api/data", (req, res) => {
     res.end(JSON.stringify({ data }));
 });
 
 app.post("/api/data", (req, res) => {
-    const chunks = [];
-    req.on("data", (chunk) => {
-        chunks.push(chunk);
-    });
-    req.on("end", () => {
-        const body = Buffer.concat(chunks).toString();
-        const parseBody = JSON.parse(body);
-        data.push(parseBody);
-        res.end(JSON.stringify({ data }));
-    });
-    return;
+    const num = req.body;
+    data.push(num);
+    res.end(JSON.stringify({ data }));
 });
 
 app.listen(8080, () => console.log("server running"));
