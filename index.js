192bc13256c26fe3709daa3f1b14564f57a71c94
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-29 12:38:28 -0300

    feat: Add route to receive and store data in an API

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const server = http.createServer((req, res) => {
         return;
     }
 
+    if (req.url === "/api/data" && req.method === "POST") {
+        const chunks = [];
+        req.on("data", (chunk) => {
+            chunks.push(chunk);
+        });
+        req.on("end", () => {
+            const body = Buffer.concat(chunks).toString();
+            const parseBody = JSON.parse(body);
+            data.push(parseBody);
+            res.end(JSON.stringify({ data }));
+        });
+        return;
+    }
+
     res.statusCode = 404;
     res.end("Not Found");
 });
