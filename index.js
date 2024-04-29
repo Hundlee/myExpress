00a9837762bcbde1e1cc37445d97dfe20fe71ef8
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-29 14:24:03 -0300

    feat: Add GET and POST routes for data manipulation
    Adds GET and POST routes for data manipulation in '/api/data'.

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const http = require("http");
+const myExpress = require("./myExpress");
 
 const data = [1, 2, 3];
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/api/data" && req.method === "GET") {
-        res.end(JSON.stringify({ data }));
-        return;
-    }
+const app = myExpress();
 
-    if (req.url === "/api/data" && req.method === "POST") {
-        const chunks = [];
-        req.on("data", (chunk) => {
-            chunks.push(chunk);
-        });
-        req.on("end", () => {
-            const body = Buffer.concat(chunks).toString();
-            const parseBody = JSON.parse(body);
-            data.push(parseBody);
-            res.end(JSON.stringify({ data }));
-        });
-        return;
-    }
-
-    res.statusCode = 404;
-    res.end("Not Found");
+app.get("/api/data", (req, res) => {
+    res.end(JSON.stringify({ data }));
 });
 
-server.listen(8080, () => console.log("server running"));
+app.post("/api/data", (req, res) => {
+    const chunks = [];
+    req.on("data", (chunk) => {
+        chunks.push(chunk);
+    });
+    req.on("end", () => {
+        const body = Buffer.concat(chunks).toString();
+        const parseBody = JSON.parse(body);
+        data.push(parseBody);
+        res.end(JSON.stringify({ data }));
+    });
+    return;
+});
+
+app.listen(8080, () => console.log("server running"));
