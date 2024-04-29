ea4d0d7808819566242778aecd6a9cef73ecc29e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-29 12:10:13 -0300

    feat: filtering the api url so that if accessing the wrong url it returns an error

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello World");
+    if (req.url === "/api/data" && req.method === "GET") {
+        res.end(JSON.stringify({ data: [1, 2, 3] }));
+        return;
+    }
+
+    res.statusCode = 404;
+    res.end("Not Found");
 });
 
 server.listen(8080, () => console.log("server running"));
